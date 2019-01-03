535a9a1ebbcac3626e9d65c2a3eff50f76249e55
Kanan Rahimov <mail@kenanbek.me>
2019-01-03 03:41:25 +0100

    Minor updates

--- crawler.py
+++ crawler.py
@@ -5,12 +5,11 @@ import requests
 response = requests.get("https://en.wikipedia.org/wiki/Python_(programming_language)")
 
 if response is not None:
-    html = bs4.BeautifulSoup(response.text, 'html.parser')
+    page = bs4.BeautifulSoup(response.text, 'html.parser')
 
-    title = html.select("#firstHeading")[0].text
-    paragraphs = html.select("p")
-    for para in paragraphs:
-        print (para.text)
-
+    title = page.select("#firstHeading")[0].text
+    paragraphs = page.select("p")
+    
+    print(title)
     intro = '\n'.join([ para.text for para in paragraphs[0:5]])
     print (intro)
